6b1c016d51eeff48ceae809283b74549ef343c68
SIX Operator <xf02id1@xf02id1-ws2.cs.nsls2.local>
2019-09-29 09:25:33 -0400

    2019-09-29_09-25-33: Automatic commit at SIX

--- 304795_Dietl.py
+++ 304795_Dietl.py
@@ -1,3 +1,77 @@
+
+
+
+
+def energy_462_thin_CRO_84K_v13():
+    ext_vg = 30
+    total_time = 600
+    cyc = 45
+    #    yield from rixs_one_energy_1(sec_x_pt,total_time,cyc,i,ext_vg, 'E_map')
+    yield from pol_H()
+    E = list(np.arange(462,462.5,1.0))
+    for i in E:
+        sec_x_pt = 5
+        yield from rixs_one_energy_1(sec_x_pt,total_time,cyc,i,ext_vg, 'E_map_thin_film')
+
+
+def energy_462_thin_CRO_84K_v12():
+    ext_vg = 30
+    total_time = 600
+    cyc = 15
+    #    yield from rixs_one_energy_1(sec_x_pt,total_time,cyc,i,ext_vg, 'E_map')
+    yield from pol_H()
+    E = list(np.arange(462,462.5,1.0))
+    for i in E:
+        sec_x_pt = 5
+        yield from rixs_one_energy_1(sec_x_pt,total_time,cyc,i,ext_vg, 'E_map_thin_film')
+
+def energy_462_thin_CRO_84K_v11():
+    ext_vg = 30
+    total_time = 600
+    cyc = 12
+    #    yield from rixs_one_energy_1(sec_x_pt,total_time,cyc,i,ext_vg, 'E_map')
+    yield from pol_H()
+    E = list(np.arange(462,462.5,1.0))
+    for i in E:
+        sec_x_pt = 5
+        yield from rixs_one_energy_1(sec_x_pt,total_time,cyc,i,ext_vg, 'E_map_thin_film')
+
+
+def energy_462_thin_CRO_84K_v10():
+    ext_vg = 30
+    total_time = 600
+    cyc = 49
+    #    yield from rixs_one_energy_1(sec_x_pt,total_time,cyc,i,ext_vg, 'E_map')
+    yield from pol_H()
+    E = list(np.arange(462,462.5,1.0))
+    for i in E:
+        sec_x_pt = 5
+        yield from rixs_one_energy_1(sec_x_pt,total_time,cyc,i,ext_vg, 'E_map_thin_film')
+
+
+def energy_462_thin_CRO_84K_v9():
+    ext_vg = 30
+    total_time = 600
+    cyc = 19
+    #    yield from rixs_one_energy_1(sec_x_pt,total_time,cyc,i,ext_vg, 'E_map')
+    yield from pol_H()
+    E = list(np.arange(462,462.5,1.0))
+    for i in E:
+        sec_x_pt = 5
+        yield from rixs_one_energy_1(sec_x_pt,total_time,cyc,i,ext_vg, 'E_map_thin_film')
+
+
+def energy_462_thin_CRO_85K_v8():
+    ext_vg = 30
+    total_time = 600
+    cyc = 24
+    #    yield from rixs_one_energy_1(sec_x_pt,total_time,cyc,i,ext_vg, 'E_map')
+    yield from pol_H()
+    E = list(np.arange(462,462.5,1.0))
+    for i in E:
+        sec_x_pt = 5
+        yield from rixs_one_energy_1(sec_x_pt,total_time,cyc,i,ext_vg, 'E_map_thin_film')
+
 def energy_462_thin_CRO_90K_v7():
     ext_vg = 30
     total_time = 600
